436d0a65007c5026cee4d89fe1529ad57edfe91a
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-06-05 12:17:49 +0530

    Adding new directory

---
+++ Learning React/class-07/props/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <Card username="Laxman Krishnamurti" btnText="Profile" />
+      <Card username="Harshad Mehta" btnText="Porfolio" />
+      <Card username="Harshad Mehta" />
+    </>
+  )
+}
+
+export default App
