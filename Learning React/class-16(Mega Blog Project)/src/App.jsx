6761e58af7cb207f932878ca42be456c556c4661
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-06-15 16:03:57 +0530

    Adding mega blog project

---
+++ Learning React/class-16(Mega Blog Project)/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-violet-500 p-4 text-center text-white font-bold text-2xl font-sans'>Mega Blog Project</h1>
+    </>
+  )
+}
+
+export default App
