0221d404a2d63d6cf9fd9dcc605349f007a12e7d
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-06-15 16:19:53 +0530

    Making changes iin the file

--- Learning React/class-16(Blog Project)/src/App.jsx
+++ Learning React/class-16(Blog Project)/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1 className="bg-violet-500 text-center text-white font-bold text-2xl p-4">Mega Blog Project</h1>
+      <h1 className="bg-violet-500 text-center text-white font-bold text-2xl p-4 shadow-md shadow-black">Mega Blog Project</h1>
     </>
   )
 }
