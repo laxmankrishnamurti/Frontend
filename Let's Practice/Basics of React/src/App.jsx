512ee89e5a741e0a966f9616e8547b67d94a4be8
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-06-22 10:02:09 +0530

    Let's practice React

---
+++ Let's Practice/Basics of React/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-violet-500 text-center text-2+xl p-4 text-white font-bold">Basics of React</h1>
+    </>
+  )
+}
+
+export default App
