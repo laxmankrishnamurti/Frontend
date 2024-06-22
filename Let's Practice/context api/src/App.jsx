a42dfd03a5f57c9f133ccac8313a4fd0abbaf15d
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-06-22 10:38:22 +0530

    Addinge context api in let's practice directory

---
+++ Let's Practice/context api/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-red-400 text-white font-bold text-2xl p-4 text-center">Context API in React</h1>
+    </>
+  )
+}
+
+export default App
