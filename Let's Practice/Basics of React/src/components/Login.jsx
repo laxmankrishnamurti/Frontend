96d3bc3fb4e9258b1dc945670457d15224b1fcfd
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-06-23 06:20:05 +0530

    Passing props from routes

--- Let's Practice/Basics of React/src/components/Login.jsx
+++ Let's Practice/Basics of React/src/components/Login.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-function Login() {
+function Login(props) {
+    const { username, password } = props.credentials
+
     return (
-        <div>This is login section </div>
+        <div>
+            <h1>Login Credential Details</h1>
+            <p>username : {username}</p>
+            <p>Password : {password}</p>
+        </div>
     )
 }
 
--- Let's Practice/Basics of React/src/main.jsx
+++ Let's Practice/Basics of React/src/main.jsx
@@ -6,13 +6,18 @@ import { Home, About, Login, Contact, Profile } from './components/index.js'
 import { getProfile } from './components/Profile/Profile.jsx'
 import Root from './Root.jsx'
 
+const loginCredentials = {
+  username: "laxmankrishnamurti",
+  password: "letscode"
+}
+
 
 const route = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Root />}>
       <Route path='' element={<Home />}></Route>
       <Route path='about' element={<About />}></Route>
-      <Route path='login' element={<Login />}></Route>
+      <Route path='login' element={<Login credentials={loginCredentials} />}></Route>
       <Route path='contact' element={<Contact />}></Route>
       <Route path='profile' element={<Profile />} loader={getProfile} ></Route>
     </Route>
