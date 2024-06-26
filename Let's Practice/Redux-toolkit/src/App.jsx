8c3a3cf43a7fadbafb860a36f81b17ab893a91c3
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-06-27 01:02:41 +0530

    Setting up redux-toolkit enviroment

---
+++ Let's Practice/Redux-toolkit/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-purple-500 text-3xl font-bold p-4 text-center text-white'>Redux toolkit</h1>
+    </>
+  )
+}
+
+export default App
