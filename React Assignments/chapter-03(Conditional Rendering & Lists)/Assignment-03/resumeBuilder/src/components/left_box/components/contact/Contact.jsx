1b80c73d2ef125f8e6c76279ef5ecbd5acdd12b8
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-05 05:26:09 +0530

    assignment three of chapter-03 is done

--- React Assignments/chapter-03(Conditional Rendering & Lists)/Assignment-03/resumeBuilder/src/components/left_box/components/contact/Contact.jsx
+++ React Assignments/chapter-03(Conditional Rendering & Lists)/Assignment-03/resumeBuilder/src/components/left_box/components/contact/Contact.jsx
@@ -1,28 +1,41 @@
 import React from 'react'
 
-function Contact(props) {
+function Contact({ data }) {
 
-    const contactData = props.data.contact
+    const contactData = data.contact;
 
     return (
         <div className='text-white'>
             <h1 className='text-2xl mt-4 inline-block font-bold'>Contact</h1>
             <hr />
             <ul>
-                <li className='mt-4'>
-                    <h2 className='font-bold'>Phone</h2>
-                    <span className='text-sm'>{contactData.phone}</span>
-                </li>
-                <li className='mt-4'>
-                    <h2 className='font-bold'>Email</h2>
-                    <span className='text-sm'>{contactData.email}</span>
-                </li>
-                <li className='mt-4'>
-                    <h2 className='font-bold'>Address</h2>
-                    <span className='text-sm'>{contactData.address}</span>
-                </li>
+                {
+                    contactData.map((data, index) => (
+                        <li className='mt-4' key={index}>
+                            {data.phone && (
+                                <>
+                                    <h2 className='font-bold'>Phone</h2>
+                                    <span className='text-sm'>{data.phone}</span>
+                                </>
+                            )}
+                            {data.email && (
+                                <>
+                                    <h2 className='font-bold'>Email</h2>
+                                    <span className='text-sm'>{data.email}</span>
+                                </>
+                            )}
+                            {data.address && (
+                                <>
+                                    <h2 className='font-bold'>Address</h2>
+                                    <span className='text-sm'>{data.address}</span>
+                                </>
+                            )}
+                        </li>
+                    ))
+                }
             </ul>
-        </div>
+
+        </div >
     )
 }
 
--- React Assignments/chapter-03(Conditional Rendering & Lists)/Assignment-03/resumeBuilder/src/components/left_box/components/education/Education.jsx
+++ React Assignments/chapter-03(Conditional Rendering & Lists)/Assignment-03/resumeBuilder/src/components/left_box/components/education/Education.jsx
@@ -1,38 +1,26 @@
 import React from 'react'
 
-function Education(props) {
+function Education({ data }) {
 
-    const educationData = props.data.education
+    const educationData = data.education
 
     return (
         <div className='text-white'>
             <h1 className='text-2xl mt-4 inline-block font-bold'>Education</h1>
             <hr />
             <ul>
-                <li className='mt-4'>
-                    <span>{educationData.first.degree}</span>
-                    <ul className='text-sm'>
-                        <li>{educationData.first.year}</li>
-                        <li>{educationData.first.school_name}</li>
-                        <li>{educationData.first.status}</li>
-                    </ul>
-                </li>
-                <li className='mt-4'>
-                    <span>{educationData.second.degree}</span>
-                    <ul className='text-sm'>
-                        <li>{educationData.second.year}</li>
-                        <li>{educationData.second.school_name}</li>
-                        <li>{educationData.second.status}</li>
-                    </ul>
-                </li>
-                <li className='mt-4'>
-                    <span>{educationData.third.degree}</span>
-                    <ul className='text-sm'>
-                        <li>{educationData.third.year}</li>
-                        <li>{educationData.third.school_name}</li>
-                        <li>{educationData.third.status}</li>
-                    </ul>
-                </li>
+                {
+                    educationData.map((data, index) => (
+                        <li className='mt-4' key={index}>
+                            <span>{data.degree}</span>
+                            <ul className='text-sm'>
+                                <li>{data.year}</li>
+                                <li>{data.school_name}</li>
+                                <li>{data.status}</li>
+                            </ul>
+                        </li>
+                    ))
+                }
             </ul>
         </div>
     )
