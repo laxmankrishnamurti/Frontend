1bf465231641474f839820597ee35faba4694f97
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-06 03:52:32 +0530

    assignment-07 is done

---
+++ React Assignments/chapter-04(Events)/Assignment-07/assignment.md
@@ -0,0 +1,1 @@
+## Make a custom event called onClose. this event should close the current browser tab. you can apply it to a button on click or anywhere.
---
+++ React Assignments/chapter-04(Events)/Assignment-07/closeTab/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  function onClose() {
+    window.close()
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <button onClick={onClose} className='bg-pink-600 px-8 py-2 mt-4 rounded-md text-white'>Close the Tab</button>
+    </>
+  )
+}
+
+export default App
