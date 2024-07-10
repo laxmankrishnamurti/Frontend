2596693259741b5f74b797daaa85f6942cfe057a
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-11 03:50:05 +0530

    Assignment-02 chapter-05 is completed

---
+++ React Assignments/chapter-05(State)/Assignment-02/assignment.md
@@ -0,0 +1,1 @@
+## Assignment :: Implement a simple TIMER that displays the elapsed time since the start button was pressed, and it can be stopped and reset. Like a stopwatch.
--- React Assignments/chapter-05(State)/Assignment-02/src/App.jsx
+++ React Assignments/chapter-05(State)/Assignment-02/src/App.jsx
@@ -1,33 +1,107 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+  const [running, setRunning] = useState(false)
+  const [totalSeconds, setTotalSeconds] = useState(0)
+
+  useEffect(() => {
+    let timer;
+    if (running) {
+      timer = setInterval(() => {
+        setTotalSeconds((prev) => {
+          if (prev > 0) {
+            return (prev - 1)
+          } else {
+            clearInterval(timer)
+            setRunning(false)
+            window.alert("Time's up")
+            return 0;
+          }
+        })
+      }, 1000);
+    }
+
+    return () => clearInterval(timer)
+  }, [running])
+
+  useEffect(() => {
+    setTotalSeconds((hours * 3600) + (minutes * 60) + (seconds))
+  }, [hours, minutes, seconds])
+
+  const startStopWatch = () => {
+    setRunning(true);
+  }
+
+  const stopStopWatch = () => {
+    setRunning(false)
+  }
+
+  const resetStopWatch = () => {
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+    running(false);
+    totalSeconds(0);
+  }
+
+  const displayTime = () => {
+
+    const hrs = Math.floor(totalSeconds / 3600);
+    const mins = Math.floor(totalSeconds / 60);
+    const second = (totalSeconds % 60)
+
+    return `${hrs.toString().padStart(2, '0')} :: ${mins.toString().padStart(2, '0')} :: ${second.toString().padStart(2, '0')}`
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className='text-3xl font-bold'>Stop Watch</h1>
+      <div className='mt-4'>
+        <input
+          type='range'
+          value={hours}
+          onChange={(e) => setHours(e.target.value)}
+          min={1}
+          max={24}
+        />
+        <label>Hours : {hours}</label>
+        <input
+          type='range'
+          value={minutes}
+          onChange={(e) => setMinutes(e.target.value)}
+          min={0}
+          max={59}
+        />
+        <label>Minutes : {minutes}</label>
+        <input
+          type='range'
+          value={seconds}
+          onChange={(e) => setSeconds(e.target.value)}
+          min={0}
+          max={59}
+        />
+        <label>Seconds : {seconds}</label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='mt-4'>
+        <h2>Remaining time :: {displayTime()}</h2>
+        <button
+          onClick={startStopWatch}
+          disabled={running}
+          className='px-8 py-2 bg-green-400 rounded-md mr-2 mt-4'>Start</button>
+        <button
+          onClick={stopStopWatch}
+          disabled={!running}
+          className='px-8 py-2 bg-red-400 rounded-md mr-2 mt-4'>Stop</button>
+        <button
+          onClick={resetStopWatch}
+          className='px-8 py-2 bg-blue-400 rounded-md mr-2 mt-4'>Reset</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
