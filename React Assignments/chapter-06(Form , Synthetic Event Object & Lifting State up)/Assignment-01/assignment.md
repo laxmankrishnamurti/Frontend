4c196b993d62a9daccf99807835bf2755c9c44fa
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-12 23:30:57 +0530

    Assignment -01 of chapter -06 is completed

---
+++ React Assignments/chapter-06(Form , Synthetic Event Object & Lifting State up)/Assignment-01/assignment.md
@@ -0,0 +1,11 @@
+## Assignment
+
+Create a Dropdown (< Select >) menu which is formed by a nations array. You can push to this array new items using a 2 input textbox (for country name and code) and button. On selection of the any option from dropdown, its value should be displayed on console.log
+
+```javascript
+const nations = [
+  { name: "India", value: "IN" },
+  { name: "Pak", value: "PK" },
+  { name: "Bangladesh", value: "BG" },
+];
+```
--- React Assignments/chapter-06(Form , Synthetic Event Object & Lifting State up)/Assignment-01/src/App.jsx
+++ React Assignments/chapter-06(Form , Synthetic Event Object & Lifting State up)/Assignment-01/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [countryProps, setCountryProps] = useState([])
+  const [country, setCountry] = useState([])
+  const [name, setName] = useState('')
+  const [code, setCode] = useState('')
+
+  function handleAdd() {
+    const newCountry = {
+      [name]: code
+    }
+    setCountry([...country, { ...newCountry }])
+    setName("")
+    setCode("")
+  }
+
+  useEffect(() => {
+    country.map((count) => {
+      for (let key in count) {
+        if (countryProps.includes(key)) {
+          return
+        } else {
+          setCountryProps([...countryProps, key])
+        }
+      }
+    })
+  }, [country])
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1 className='text-4xl mb-8'>Dropdown menu</h1>
+        <div>
+          <div className='mb-4'>
+            <label htmlFor='name' className='mr-4'>Enter country name</label>
+            <input
+              type='text'
+              id='name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className='border-2 border-black px-2 py-1 outline-pink-500 rounded-md'
+            />
+          </div>
+          <div className='mb-4'>
+            <label htmlFor='code' className='mr-4'>Enter country Code</label>
+            <input
+              type='text'
+              id='code'
+              value={code}
+              onChange={(e) => setCode(e.target.value)}
+              className='border-2 border-black px-2 py-1 outline-pink-500 rounded-md'
+            />
+          </div>
+          <div className='mb-8'>
+            <button
+              className='w-64 bg-black text-white px-4 py-2 rounded-md'
+              onClick={handleAdd}
+            >Add</button>
+          </div>
+          <div>
+            {country.length > 0 ? (<select onClick={(e) => console.log(e.target.value)} className='w-40 px-4 py-2 bg-green-400 rounded-md'>
+              {countryProps.map((prop) => {
+                return <option key={prop} name={prop} value={country.map(count => count[prop])} > {country.map(count => count[prop])}</option>
+              })}
+            </select>) : null}
+          </div>
+        </div>
+      </div >
     </>
   )
 }
 
 export default App
+
+
+// {
+//   countryProps.map((prop) => (
+//     <option key={prop} name={prop} value={optionValue.map((value) => value)}>{optionValue.map(value => value)}</option>
+//   ))
+// }
