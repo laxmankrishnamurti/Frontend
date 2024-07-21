e8e47b7dcce3c05afa21269a281de6d84cfaeb5c
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-22 01:47:26 +0530

    Adding react official challenges into this chapter

--- React Assignments/chapter-10(useRef)/src/App.jsx
+++ React Assignments/chapter-10(useRef)/src/App.jsx
@@ -1,31 +1,42 @@
 import { useRef, useState } from 'react'
 
 function App() {
-  const [input, setInput] = useState('')
-  const refValue = useRef()
 
-  function handleReference() {
-    console.log("refObject :  ", refValue)
-    console.log("refValue is : ", refValue.current.value)
+  const [inputMsg, setInputMsg] = useState('')
+  const [isSending, setIsSending] = useState(false)
+  let eventId = useRef(null)
+
+  function handleMessage() {
+    setIsSending(prev => !prev)
+    eventId.current = setTimeout(() => {
+      alert("Message send successfully")
+      setIsSending(prev => !prev)
+    }, 3000);
   }
 
+  function stopMessage() {
+    setIsSending(false)
+    clearTimeout(eventId.current)
+  }
 
   return (
     <>
       <h1 className='bg-violet-400 p-4 text-center text-3xl font-bold'>useRef Hook</h1>
       <div>
         <input
-          ref={refValue}
           type='text'
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder='Enter your name'
-          className='border-2 outline-pink-500 border-black block my-4'
+          value={inputMsg}
+          onChange={(e) => setInputMsg(e.target.value)}
+          className='border-2 border-black my-4 ml-4'
         />
         <button
-          onClick={handleReference}
-          className='px-4 py-2 bg-pink-400 rounded-md '
-        >Check Reference Vlaue </button>
+          onClick={handleMessage}
+          className='px-4 py-1 bg-green-400 rounded-md ml-4'>{isSending ? "Sending..." : "Send"}</button>
+        {isSending && (
+          <button
+            onClick={stopMessage}
+            className='px-4 py-1 bg-green-400 rounded-md ml-4'>Undo</button>
+        )}
       </div>
     </>
   )
