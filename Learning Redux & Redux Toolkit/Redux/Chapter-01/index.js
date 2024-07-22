dbcd46c25c5eb465bfff6d980db197de7447dedf
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-22 18:33:44 +0530

    Understanding Basics of Redux architecture and how it works

--- Learning Redux & Redux Toolkit/Redux/Chapter-01/index.js
+++ Learning Redux & Redux Toolkit/Redux/Chapter-01/index.js
@@ -1,5 +1,25 @@
 import { createStore } from "redux";
 
-const store = createStore();
+const initialValue = {
+  name: "Laxman Krishnamurti",
+  class: 12,
+  age: 21,
+};
 
-console.log("store is :: ", store);
+const store = createStore(reducer);
+
+function reducer(store = initialValue, action) {
+  if (action.type === "incrementInAge") {
+    return { ...store, age: store.age + 1 };
+  } else if (action.type === "decrementInClass") {
+    return { ...store, class: (store.class -= 1) };
+  }
+  return store;
+}
+
+store.dispatch({ type: "incrementInAge" });
+store.dispatch({ type: "decrementInClass" });
+
+store.dispatch({ type: "incrementInAge" });
+store.dispatch({ type: "decrementInClass" });
+console.log("store is :: ", store.getState());
