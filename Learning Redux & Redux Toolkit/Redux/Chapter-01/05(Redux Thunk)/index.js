b83c8f3ceb692122818e2a0950e8cbb4150d02ae
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-23 16:29:26 +0530

    Applying thunk middleware

--- Learning Redux & Redux Toolkit/Redux/Chapter-01/05(Redux Thunk)/index.js
+++ Learning Redux & Redux Toolkit/Redux/Chapter-01/05(Redux Thunk)/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { thunk } from "redux-thunk";
 
 const initialValue = {
   _id: 1,
@@ -13,7 +14,7 @@ const incrementInAge = "incrementInAge";
 const decrementInClass = "decrementInClass";
 const updateUsername = "updateUsername";
 
-const store = createStore(reducer, applyMiddleware(logger.default));
+const store = createStore(reducer, applyMiddleware(logger.default, thunk));
 
 const history = [];
 
@@ -41,25 +42,11 @@ function handleClassDecrement(payload) {
   store.dispatch({ type: decrementInClass, payload: payload });
 }
 
-async function handleUsername() {
+async function handleUsername(dispatch, getState) {
   const { data } = await axios.get("http://localhost:3000/accounts/");
-  // for (let i = 0; i < data.length; i++) {
-  //   if (data[i].email === String("laxmankrishnamurti@gmail.com")) {
-  //     store.dispatch({ type: updateUsername, payload: data[i].username });
-  //   }
-  // }
-  // return store.dispatch({ type: updateUsername, payload: data[0].username });
-  return { type: updateUsername, payload: data[0].username };
+  dispatch({ type: updateUsername, payload: data[0].username });
 }
 
 setInterval(() => {
-  // store.dispatch({ type: decrementInClass });
-  // store.dispatch({ type: updateUsername, payload: "Harshad" });
-  // store.dispatch({ type: incrementInAge, payload: 2 });
-  // handleAgeIncrement();
-  // handleClassDecrement(3);
-  // handleUsername("Harshad Mehta");
-  // handleUsername();
-  store.dispatch(handleUsername()); //It works Synchronously it will not going to wait for any async action,
-  //err :- Action must be plain ojects. Insted, the actual type was : "promise". You may need to add middleware to your store setup to handle dispatching other values, such as 'redux-thunk' to handle dispatching functions.
-}, 1000);
+  store.dispatch(handleUsername);
+}, 5000);
