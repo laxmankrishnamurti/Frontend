805c65738ab1ba406f2c0168812e3a48778c65f6
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-28 15:33:57 +0530

    Now, I understood how to manage state in React

---
+++ Learning To Manage State In React/Context API copy/src/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header/Header";
+import InputForm from "./InputForm/InputForm";
+import TodoList from "./TodoList/TodoList";
+import Footer from "./Footer/Footer";
+
+export { Header, InputForm, TodoList, Footer };
