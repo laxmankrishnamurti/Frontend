d75aedcc277f5ac3d5d3df6b049dcffaf86ef31d
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-08-05 19:16:36 +0530

    Managing state with Context API & useReducer

--- Learning To Manage State In React/Context API With useReducer/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Header from "./Header/Header";
-import InputForm from "./InputForm/InputForm";
-import TodoList from "./TodoList/TodoList";
-import Footer from "./Footer/Footer";
-
-export { Header, InputForm, TodoList, Footer };
