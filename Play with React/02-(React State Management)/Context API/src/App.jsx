65ed4055eda8fc87778020c7c1a5388ded05612a
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-08-30 04:17:15 +0530

    Practice on state management in react

---
+++ Play with React/02-(React State Management)/Context API/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-pink-400 text-center p-4 font-bold text-white text-2xl">
+        State Management with Context API
+      </h1>
+    </>
+  );
+}
+
+export default App;
