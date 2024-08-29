76e7345890409de91427c000c32202c7cebf1128
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-08-29 20:04:00 +0530

    making chages in the code

--- Learning React/class-13(Theme Switcher)/src/App.jsx
+++ Learning React/class-13(Theme Switcher)/src/App.jsx
@@ -1,43 +1,41 @@
-import { useState } from "react"
-import { useEffect } from "react"
-import { ThemeProvider } from "./context/theme"
-import ThemeBtn from "./components/ThemeBtn"
-import Card from "./components/Card"
+import { useState } from "react";
+import { useEffect } from "react";
+import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
-
-  const [themeMode, setThemeMode] = useState("light")
+  const [themeMode, setThemeMode] = useState("light");
 
   const lightTheme = () => {
-    setThemeMode("light")
-  }
+    setThemeMode("light");
+  };
 
   const darkTheme = () => {
-    setThemeMode("dark")
-  }
+    setThemeMode("dark");
+  };
 
-  // Manipulating dom from dark to light or light to dark 
+  // Manipulating dom from dark to light or light to dark
 
   useEffect(() => {
-    document.querySelector('html').classList.remove("light", "dark")
-    document.querySelector('html').classList.add(themeMode)
-  }, [themeMode])
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
     <>
-
-
       <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
         <div>
-          <h1 className="p-4 text-center font-bold text-2xl shadow shadow-black dark:bg-black dark:text-white dark:shadow-blue-600">Theme Switcher</h1>
+          <h1 className="p-4 text-center font-bold text-2xl shadow shadow-black dark:bg-black dark:text-white dark:shadow-blue-600">
+            Theme Switcher
+          </h1>
           <div className="absolute top-5 right-4">
             <ThemeBtn />
           </div>
         </div>
         <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
-            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            </div>
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
 
             <div className="w-full max-w-sm mx-auto">
               <Card />
@@ -46,7 +44,7 @@ function App() {
         </div>
       </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
