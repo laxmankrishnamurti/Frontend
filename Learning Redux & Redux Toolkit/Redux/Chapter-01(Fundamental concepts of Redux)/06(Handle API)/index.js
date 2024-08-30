31432b45ab812d5338a19a5667a5c7efcc1ad162
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-08-30 15:54:43 +0530

    Managing state with redux

--- Learning Redux & Redux Toolkit/Redux/Chapter-01(Fundamental concepts of Redux)/06(Handle API)/index.js
+++ Learning Redux & Redux Toolkit/Redux/Chapter-01(Fundamental concepts of Redux)/06(Handle API)/index.js
@@ -24,7 +24,6 @@ function userReducer(state = initialState, action) {
     case getAccUserFulFilled:
       return {
         ...state,
-        id: action.payload.id,
         username: action.payload.username,
         age: action.payload.age,
         email: action.payload.email,
@@ -43,8 +42,8 @@ function getUserAccount(id) {
   return async (dispatch, getState) => {
     try {
       dispatch(getUserAccountPending());
-      const { data } = await axios.get(`http://localhost:3000/accounts/${id}`);
-      dispatch(getUserAccountFulFilled(data));
+      const { data } = await axios.get(`http://localhost:3000/accounts/`);
+      dispatch(getUserAccountFulFilled(data[4]));
     } catch (error) {
       getUserAccountRejected(error.message);
     }
@@ -59,7 +58,6 @@ function getUserAccountFulFilled(data) {
   return {
     type: getAccUserFulFilled,
     payload: {
-      id: data.id,
       username: data.username,
       email: data.email,
       age: data.age,
@@ -72,5 +70,5 @@ function getUserAccountRejected(error) {
 }
 
 setTimeout(() => {
-  store.dispatch(getUserAccount("37a4"));
+  store.dispatch(getUserAccount());
 }, 2000);
