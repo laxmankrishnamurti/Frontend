27d72e11324af50bc31cf68412c8d6970ed1349d
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-09-03 16:44:10 +0530

    Practice on RTK Query

--- Play with React/02-(React State Management)/Redux Toolkit (RTK Query)/src/App.jsx
+++ Play with React/02-(React State Management)/Redux Toolkit (RTK Query)/src/App.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react'
 function App() {
   const [userData, setUserData] = useState({})
   const { data, isError, isFetching } = useGetUserByIdQuery('1')
-  console.log(useGetUserByIdQuery('2'))
+
   useEffect(() => {
     if (!isFetching && data) {
       setUserData(data)
@@ -14,7 +14,9 @@ function App() {
 
   return (
     <>
-      <p>Username is : {userData.name}</p>
+      {isError ? (<p>Oops..... Something went wrong</p>) : isFetching ? (<p>Fetching</p>) : userData ? (
+        <p>Username is : {userData.name}</p>
+      ) : null}
     </>
   )
 }
